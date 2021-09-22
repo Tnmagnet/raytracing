8e221f181e24cc948acba9fadf1f08372e12a788
Tnmagnet <zhaojunyi10@outlook.com>
2021-09-22 18:08:18 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/weekend.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/weekend.dir/main.cpp.obj"
+  "libweekend.dll.a"
+  "weekend.exe"
+  "weekend.exe.manifest"
+  "weekend.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/weekend.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ rtw_stb_image.h
@@ -0,0 +1,23 @@
+//
+// Created by win10 on 2021/9/20.
+//
+
+#ifndef WEEKEND_RTW_STB_IMAGE_H
+#define WEEKEND_RTW_STB_IMAGE_H
+// Disable pedantic warnings for this external library.
+#ifdef _MSC_VER
+// Microsoft Visual C++ Compiler
+#pragma warning (push, 0)
+#endif
+
+#define STB_IMAGE_IMPLEMENTATION
+
+#include "external/stb_image.h"
+
+// Restore warning levels.
+#ifdef _MSC_VER
+// Microsoft Visual C++ Compiler
+#pragma warning (pop)
+#endif
+
+#endif //WEEKEND_RTW_STB_IMAGE_H
